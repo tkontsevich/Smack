27594c20f2c773ccd39890824694363b0c6f6319
Tanya Kontsevich <tanya@Tanyas-MacBook-Pro.local>
2018-11-04 23:51:57 -0800

    fixing ktlint issues

--- app/src/androidTest/java/com/example/tkontsevich/smack/Tests/BaseTest.kt
+++ app/src/androidTest/java/com/example/tkontsevich/smack/Tests/BaseTest.kt
@@ -2,9 +2,6 @@ package com.example.tkontsevich.smack.Tests
 
 import com.example.tkontsevich.smack.Screens.HomeScreen
 import com.example.tkontsevich.smack.Screens.NavigationScreen
-import com.example.tkontsevich.smack.Services.AuthService.createUser
-import com.example.tkontsevich.smack.Services.AuthService.loginUser
-import com.example.tkontsevich.smack.Services.AuthService.registerUser
 import org.apache.commons.lang3.RandomStringUtils
 
 open class BaseTest {
@@ -12,7 +9,6 @@ open class BaseTest {
     val existingUserEmail = "testuser@testsmack.com"
     val existingUserPsw = "123456"
     val channelName = "" + RandomStringUtils.randomAlphanumeric(5)
-
 
     fun loginAsExistingUser(): NavigationScreen {
         val homeScreen = HomeScreen()
